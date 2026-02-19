1f8c46d1d223e9fcfbe5e34b6e0cb3e6545b1f7d
BrianOldenzeel <blianoldenzeel@gmail.com>
2026-02-19 11:08:29 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'mijngeheimesleutel'
+
+class InfoForm(FlaskForm):
+
+    instrument = StringField('Welk instrument wil je graag leren bespelen?')
+    submit = SubmitField('Verzend')
+
+@app.route("/", methods=['POST', 'GET'])
+def testpage():
+    instrument = False
+    # Maak een object van de klasse InfoForm aan.
+    form = InfoForm()
+    # Als het formulier valide is
+    if form.validate_on_submit():
+        # Haal de data voor instrument op uit het formulier.
+        instrument = form.instrument.data
+        # Zet de waarde voor de variabele instrument op het formulier weer op False
+        form.instrument.data = ''
+        return render_template('main.html', form=form, instrument=instrument)
+    return render_template('main.html', form=form)
+
+
+@app.route("/kebab")
+def kebabpage():
+    return "<h1>kebab is lekker</h1>"
