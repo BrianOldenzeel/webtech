08fd98e6b1bc1783940aab3f83caa1f45cd6bf54
BrianOldenzeel <blianoldenzeel@gmail.com>
2026-03-12 09:59:53 +0100

    db conn added

--- main.py
+++ main.py
@@ -4,12 +4,34 @@ from wtforms import StringField, SubmitField
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'mijngeheimesleutel'
 
-class InfoForm(FlaskForm):
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
-    instrument = StringField('Welk instrument wil je graag leren bespelen?')
-    submit = SubmitField('Verzend')
+db = SQLAlchemy(app)
+
+class student(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    voornaam = db.Column(db.String(100))
+    achternaam = db.Column(db.String(100))
+
+class instelling(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    naam = db.Column(db.String(100))
+    soort = db.Column(db.String(100))
+
+class begeleider(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    voornaam = db.Column(db.String(100))
+    achternaam = db.Column(db.String(100))
+
+class stage(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    student_id = db.Column(db.Integer, db.ForeignKey("student.id"))
+    begeleider_id = db.Column(db.Integer, db.ForeignKey("begeleider.id"))
+    instelling_id = db.Column(db.Integer, db.ForeignKey("instelling.id"))
+    cijfer = db.Column(db.Integer)
+    periode = db.Column(db.String(100))
 
 @app.route("/", methods=['POST', 'GET'])
 def testpage():
@@ -20,4 +42,6 @@ def loginpage():
     return render_template("/auth/login.html")
 
 if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
     app.run(debug=True)
